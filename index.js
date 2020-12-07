0101dd08a8ce53ec5a39ed8ab859c5699e557ded
Cristian Franco <cristianfrancod@gmail.com>
2020-12-07 09:42:26 -0700

    dotenv dependecy added to read .env file values, mongoose and mongodb dependencies added as well and connection to the cluster's database

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+require('dotenv').config();
+
+var mongoose = require('mongoose');
+
+//makes the conecction whith the database (Mongo Atlas) the MONGO_URI value is in .env file
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+
 
 //use stactic assets for every file
 app.use(express.static(__dirname +'/public'));
@@ -11,6 +21,9 @@ app.use(function(req,res,next){
   next();
   
   });
+
+app.use(bodyParser.urlencoded({extended: false}));
+
   
 
 
@@ -24,6 +37,9 @@ app.get('/createMenu',function(req,res){
 });
 
 
+app.get('/menu/:id',function(req,res){
+  console.log(req.params.id);
+});
 
   app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
