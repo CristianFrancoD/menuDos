64564e8bda189656d9524cd7cb18b7afa6d6ed52
Cristian Franco <cristianfrancod@gmail.com>
2020-12-07 22:43:34 -0700

    create menus and find menus added

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
 require('dotenv').config();
 
 var mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const menuSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  desc: String
+});
+
+
+const Menu = mongoose.model("Menu", menuSchema);
 
 //makes the conecction whith the database (Mongo Atlas) the MONGO_URI value is in .env file
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -13,6 +25,9 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 //use stactic assets for every file
 app.use(express.static(__dirname +'/public'));
+
+
+
 
 //logger for al requests
 app.use(function(req,res,next){
@@ -26,19 +41,51 @@ app.use(bodyParser.urlencoded({extended: false}));
 
   
 
-
-app.get('/menu',function(req,res){
-   
-  res.sendFile(__dirname+'/views//menu.html');
+//Show Menus
+app.get('/menus',function(req,res){
+   Menu.find().exec(function(err,menus){
+     if(err){
+       return console.log(err);
+     }else{
+       console.log(menus);
+       res.json(menus);
+     }
+   })
 });
 
+//Show view to create Menus
 app.get('/createMenu',function(req,res){
   res.sendFile(__dirname+'/views/cmenu.html');
 });
 
+//Create Menus
+app.post('/createMenu',function(req,res){
+  let menu = new Menu(
+    {name:req.body.name,
+     price:req.body.price,
+     desc:req.body.desc});
 
+     menu.save(function(err,menu){
+       if(err){
+        return console.log(err);
+       }else{
+         console.log(menu);
+         res.redirect("/createMenu");
+       }
+     });
+});
+
+
+//Show specific menu
 app.get('/menu/:id',function(req,res){
   console.log(req.params.id);
+  Menu.findById({_id:req.params.id},function(err,menu){
+    if(err){
+      return console.log(err);
+    }else{
+      console.log(menu);
+    }
+  })
 });
 
   app.listen(3000, function () {
